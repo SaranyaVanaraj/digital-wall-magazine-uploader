e0edba6c4b8696973c5388aa8c059913e9b397e0
Saranya V <saranyavanaraj20@gmail.com>
2025-11-08 16:43:56 +0530

    DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,23 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/digitalmagazineuploader";
+    private static final String USER = "root"; // your MySQL username
+    private static final String PASSWORD = "Saran@2007"; // your MySQL password
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✅ Database connected successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
+
